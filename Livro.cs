690a6ddc1bf1b9d4382cae0bbeb4ff54596e5a74
rpereiraCidenai <148018251+rpereiraCidenai@users.noreply.github.com>
2024-11-29 11:20:47 +0000

    teste m9 - poo - métodos atributos classes

---
+++ Livro.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COR_TEXT_M9
+{
+
+
+    internal class Livro
+    {
+        /*   1 qestao do teste ok 
+    +-------------------------------------------+
+| - titulo: string | // Atributo privado
+| - autor: string | // Atributo privado
+| - anoPublicacao: int | // Atributo privado
++-----------------------------------------
+
+    */
+        private string _titulo;
+        private string _autor;
+        private int _anoPublicacao;
+ /*
+  * 
+  * + getTitulo(): string | // Método público para obter título X
+| + setTitulo(titulo: string): void | // Método público para definir título X
+| + getAutor(): string | // Método público para obter autor X 
+| + setAutor(autor: string): void | // Método público para definir autorx
+| + getAnoPublicacao(): int | // Método público para obter ano de publicação X 
+| + setAnoPublicacao(ano: int): void | // Método público para definir ano de publicação com validaçãox
+| + Livro(titulo, autor, ano) | // Construtor públicox
+| + ExibirInformacoes(): void | // Método público para exibir informações
+ * 
+ */
+
+        public string getTitulo()
+        {
+
+            return _titulo;
+        }
+        public string getAutor()
+        {
+            return _autor;
+        }
+        public int getAnoPublicacao()
+        {
+            return _anoPublicacao;
+        }
+
+        //setTitulo(titulo: string): void
+
+        public void setTitulo(string titulo)
+        {
+            _titulo = titulo;
+        }
+
+        public void setAutor(string autor   )
+        {
+            _autor = autor;
+        }
+
+        //| + setAnoPublicacao(ano: int): void | // Método público para definir ano de publicação com validação
+
+        public void setAnoPublicacao(int ano)
+        {
+            if (ano > 0)
+            {
+                _anoPublicacao = ano;
+            }
+            else
+                Console.WriteLine("nao pode inserir anos <=0");
+        }
+
+        public Livro(string titulo="", string autor="", int anoPublicacao=-1)
+        {
+            _titulo = titulo;
+            _autor = autor;
+            _anoPublicacao = anoPublicacao;
+        }
+
+        public void ExibirInformacoes()
+        {
+            Console.WriteLine("Título: " +_titulo+ " , Autor: " + _autor + " , Ano: " + _anoPublicacao);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿namespace COR_TEXT_M9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Livro livro = new Livro();
+            
+            Console.WriteLine("bom dia alunos de psi");
+            Console.WriteLine("insira o titulo do livro ");
+            livro.setTitulo( Convert.ToString(Console.ReadLine()));
+            Console.WriteLine("insira o autor do livro ");
+            livro.setAutor(Convert.ToString(Console.ReadLine()));
+            Console.WriteLine("insira o ano do livro ");
+            livro.setAnoPublicacao(Convert.ToInt32(Console.ReadLine()));
+
+            livro.ExibirInformacoes();
+
+
+        }
+    }
+}
